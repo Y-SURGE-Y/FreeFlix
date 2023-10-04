c99532cf1973dc35c67be3e3c0c874a62f7ba837
Y-Surge-Y <anand.krishn2020@gmail.com>
2023-10-05 02:41:17 +0530

    Final Build of the Netflix Clone

---
+++ src/Requests.jsx
@@ -0,0 +1,11 @@
+const key = '9a0a540140e5b9e03f5045caa70756a0'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests
